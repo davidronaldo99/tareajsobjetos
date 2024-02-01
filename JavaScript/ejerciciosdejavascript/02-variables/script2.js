77d6b94a04339df564b35ce6a04f2a618f656e4c
davidronaldo99 <151529130+davidronaldo99@users.noreply.github.com>
2024-02-01 11:27:43 +0100

    Add files via upload

---
+++ JavaScript/ejerciciosdejavascript/02-variables/script2.js
@@ -0,0 +1,7 @@
+let nombre = "David";
+let apellidos = "Garcia ";
+let animal = "Pantera";
+
+console.log("Nombre:", nombre);
+console.log("Apellidos:", apellidos);
+console.log("Animal Preferido:", animal);
